c476e9105ff276a15b3b021766d830fd7934366c
kleinbrey <1427784361@qq.com>
2023-02-23 19:36:05 +0800

    223

--- src/app/pages/basic/form/form.component.ts
+++ src/app/pages/basic/form/form.component.ts
@@ -1,99 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  Observable,
-  firstValueFrom,
-  lastValueFrom,
-  Subject,
-  BehaviorSubject,
-  ReplaySubject,
-  from,
-  fromEvent,
-  repeat,
-  timer,
-} from 'rxjs';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Observable, firstValueFrom, lastValueFrom, Subject, BehaviorSubject, ReplaySubject, from, fromEvent, repeat, timer } from 'rxjs';
 import { of, interval } from 'rxjs';
 import { filter, map, take } from 'rxjs/operators';
 import moment from 'moment';
 import { DestroySubjectService } from 'src/service';
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.less'],
-  providers: [DestroySubjectService]
+    selector: 'app-form',
+    templateUrl: './form.component.html',
+    styleUrls: ['./form.component.less'],
+    providers: [DestroySubjectService]
 })
-export class FormComponent implements OnInit {
-  public autho!: string;
-  datarange: any;
-  constructor() { }
-  async ngOnInit(): Promise<void> {
-    const observaber = {
-      next: (val: any) => console.log(val),
-      error: (error: any) => console.log(error),
-      complete: () => console.log('完成'),
-    };
-    const from = 'Wed Aug 24 2022 20:11:43 GMT+0800';
-    const end = 'Wed Aug 24 2022 20:11:43 GMT+0800';
-    console.log(moment(from).diff(end));
+export class FormComponent implements OnInit, AfterViewInit {
+    @ViewChild('appHighlight') appHighlight: any;
+    data: any = {value:'12345'};
+    datarange: any;
+    constructor() {}
+    async ngOnInit(): Promise<void> {
+        const observaber = {
+            next: (val: any) => console.log(val),
+            error: (error: any) => console.log(error),
+            complete: () => console.log('完成')
+        };
+        const from = 'Wed Aug 24 2022 20:11:43 GMT+0800';
+        const end = 'Wed Aug 24 2022 20:11:43 GMT+0800';
+        console.log(moment(from).diff(end));
+        // const createObservable = (observaber: any) => {
+        //   let number = 1;
+        //   const time = setInterval(() => {
+        //     observaber.next(number++);
+        //   }, 1000);
+        //   return {
+        //     unsubscribe: () => {
+        //       clearInterval(time);
+        //     },
+        //   };
+        // };
+        // const myobservable = new Observable(createObservable);
 
-    // const createObservable = (observaber: any) => {
-    //   let number = 1;
-    //   const time = setInterval(() => {
-    //     observaber.next(number++);
-    //   }, 1000);
-    //   return {
-    //     unsubscribe: () => {
-    //       clearInterval(time);
-    //     },
-    //   };
-    // };
-    // const myobservable = new Observable(createObservable);
+        // const subscription = myobservable.subscribe(observaber);
+        // console.log(subscription);
+        // setTimeout(() => {
+        //   subscription.unsubscribe();
+        // }, 5000);
 
-    // const subscription = myobservable.subscribe(observaber);
-    // console.log(subscription);
-    // setTimeout(() => {
-    //   subscription.unsubscribe();
-    // }, 5000);
+        // const last = await lastValueFrom(myobservable);
 
-    // const last = await lastValueFrom(myobservable);
+        // const first = await firstValueFrom(myobservable);
+        // console.log(myobservable,first);
+        // console.log(myobservable, last);
 
-    // const first = await firstValueFrom(myobservable);
-    // console.log(myobservable,first);
-    // console.log(myobservable, last);
+        // const fromObservable = from([1, 2, 3]);
+        // fromObservable.subscribe(observaber);
 
-    // const fromObservable = from([1, 2, 3]);
-    // fromObservable.subscribe(observaber);
+        // const el = document.querySelectorAll('#top');
+        // const fromevent = fromEvent(el, 'click');
+        // fromevent.subscribe(observaber)
 
-    // const el = document.querySelectorAll('#top');
-    // const fromevent = fromEvent(el, 'click');
-    // fromevent.subscribe(observaber)
+        // const ofObservable = from([1, 2, 3]).pipe(repeat(3));
+        // ofObservable.subscribe(observaber);
 
-    // const ofObservable = from([1, 2, 3]).pipe(repeat(3));
-    // ofObservable.subscribe(observaber);
-
-    // const timerOb = timer(5000, 1000).pipe(map((x) => x + 10));
-    // timerOb.subscribe(observaber);
-  }
-
-  buttonshow(val: any) {
-    alert(val);
-  }
-  // buttonshow() {
-  //   this.storeService.execute()
-  // }
-
-  //@ts-ignore
-  // changeLastData(num: number) {
-  //   const last = this.adf.slice(-1)[0]
-  //   console.log('00');
-
-  //   if (num - last < 2) {
-
-  //     return this.adf
-  //   }
-  //   this.adf.push(last + 2)
-  //   return this.changeLastData(num)
-  // }
-
-
+        // const timerOb = timer(5000, 1000).pipe(map((x) => x + 10));
+        // timerOb.subscribe(observaber);
+    }
+    ngAfterViewInit(): void {
+        console.log(this.appHighlight);
+    }
+    buttonshow(val: any) {
+        alert(val);
+    }
 }
-1814403499;
--- src/directive/highLightDirective.ts
+++ src/directive/highLightDirective.ts
@@ -1,35 +1,29 @@
-import {
-  Directive,
-  ElementRef,
-  HostListener,
-  HostBinding,
-  OnInit,
-} from '@angular/core';
+import { Directive, ElementRef, HostListener, HostBinding, OnInit } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlight]',
+    selector: '[appHighlight]',
+    // 加上 exportAs 来获取指令实例
+    exportAs: 'appHighlight'
 })
 export class HighlightDirective implements OnInit {
-  constructor(private el: ElementRef) { }
+    constructor(private el: ElementRef) {}
 
- 
-  @HostListener('mouseenter') onMouseEnter() {
-    this.highlight('yellow');
-  }
+    @HostListener('mouseenter') onMouseEnter() {
+        this.highlight('yellow');
+    }
 
-  @HostListener('mouseleave') onMouseLeave() {
-    this.highlight('');
-  }
+    @HostListener('mouseleave') onMouseLeave() {
+        this.highlight('');
+    }
 
-  @HostBinding('style.color') color!: string;
+    @HostBinding('style.color') color!: string;
 
-  ngOnInit() {
-    console.log("自定义指令");
-
-  }
-  highlight(color: string) {
-    // 两种方法改变dom上的元素
-    // this.el.nativeElement.style.backgroundColor = color;
-    this.color = color;
-  }
+    ngOnInit() {
+        console.log('自定义指令');
+    }
+    highlight(color: string) {
+        // 两种方法改变dom上的元素
+        // this.el.nativeElement.style.backgroundColor = color;
+        this.color = color;
+    }
 }
